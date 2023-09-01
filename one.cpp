257613a1dfa38698a95b80026d78cff77375e329
SHI HUAYUE <1315126403@qq.com>
2023-09-01 20:55:44 +0800

    Signed-off-by: SHI HUAYUE <1315126403@qq.com>

--- one.cpp
+++ one.cpp
@@ -11,4 +11,25 @@ void common_divivor_multiple(int a, int b)
 		a = b;
 	}
 	cout << a << tm1 / a;
+}int non_recurion_fast_exponen(int num, int times)
+{
+	int resul = num;
+	while (times)
+	{
+		if (times & 1)
+		{
+			resul = resul * num;
+			times = times - 1;
+		}
+		else
+		{
+			resul *= resul;
+			times = times / 2;
+		}
+	}
+	return resul;
+}
+int recurion_fast_exponen(int num, int times)
+{
+
 }
